6cb8dde809bdc3a211e1b792b08e93ecfac74244
Akshara2007 <perkaakshara3@gmail.com>
2026-06-29 12:10:30 +0530

    Added initial digital clock Verilog design

--- digital_clock.v
+++ digital_clock.v
@@ -1,1 +1,53 @@
+module digital_clock(
+    input clk,
+    input reset,
+    output reg [5:0] seconds,
+    output reg [5:0] minutes,
+    output reg [4:0] hours
+);
 
+reg [25:0] count;
+
+always @(posedge clk)
+begin
+    if(reset)
+    begin
+        count <= 0;
+        seconds <= 0;
+        minutes <= 0;
+        hours <= 0;
+    end
+
+    else if(count == 50000000-1)
+    begin
+        count <= 0;
+
+        if(seconds == 59)
+        begin
+            seconds <= 0;
+
+            if(minutes == 59)
+            begin
+                minutes <= 0;
+
+                if(hours == 23)
+                    hours <= 0;
+                else
+                    hours <= hours + 1;
+            end
+
+            else
+                minutes <= minutes + 1;
+        end
+
+        else
+            seconds <= seconds + 1;
+    end
+
+    else
+    begin
+        count <= count + 1;
+    end
+end
+
+endmodule
