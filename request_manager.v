9c34e9a2e98f51bf31b62a9dd691aebe62d1a302
Akshara2007 <perkaakshara3@gmail.com>
2026-07-08 16:37:50 +0530

    Update request_manager.v

--- request_manager.v
+++ request_manager.v
@@ -1,1 +1,41 @@
+`timescale 1ns/1ps
 
+module request_manager(
+
+    input clk,
+    input reset,
+
+    input request_valid,
+    input [1:0] requested_floor,
+
+    input clear_request,
+    input [1:0] served_floor,
+
+    output reg [3:0] requests
+
+);
+
+always @(posedge clk or posedge reset)
+begin
+
+    if(reset)
+    begin
+        requests <= 4'b0000;
+    end
+
+    else
+    begin
+
+        // Store new request
+        if(request_valid)
+            requests[requested_floor] <= 1'b1;
+
+        // Clear completed request
+        if(clear_request)
+            requests[served_floor] <= 1'b0;
+
+    end
+
+end
+
+endmodule
