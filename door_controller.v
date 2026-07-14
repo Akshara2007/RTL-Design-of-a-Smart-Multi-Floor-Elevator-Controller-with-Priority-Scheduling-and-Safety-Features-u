b1f6093e06134e06027914a8189b7e39267d61aa
Akshara2007 <perkaakshara3@gmail.com>
2026-07-14 15:55:11 +0530

    Update door_controller.v

--- door_controller.v
+++ door_controller.v
@@ -6,132 +6,97 @@ module door_controller(
     input reset,
 
     input open_request,
-    input obstruction,
 
-    output reg door_open,
-    output reg door_close,
-    output reg door_busy
+    output reg door_open
 
 );
 
 
-parameter CLOSED = 2'b00;
-parameter OPEN   = 2'b01;
-parameter WAIT   = 2'b10;
-parameter CLOSE  = 2'b11;
+    reg [1:0] state;
+    reg [31:0] counter;
 
 
-reg [1:0] state;
-reg [2:0] timer;
+    // Door states
+    parameter CLOSED = 2'b00;
+    parameter OPEN   = 2'b01;
+    parameter WAIT   = 2'b10;
 
 
-always @(posedge clk or posedge reset)
-begin
 
-    if(reset)
-    begin
-        state <= CLOSED;
-        door_open <= 0;
-        door_close <= 0;
-        door_busy <= 0;
-        timer <= 0;
-    end
-
-
-    else
+    always @(posedge clk or posedge reset)
     begin
 
-        case(state)
-
-
-        // Door closed
-        CLOSED:
+        if(reset)
         begin
-
-            door_open <= 0;
-            door_close <= 0;
-            door_busy <= 0;
-
-
-            if(open_request)
-            begin
-                state <= OPEN;
-            end
-
+            door_open <= 1'b0;
+            counter <= 32'd0;
+            state <= CLOSED;
         end
 
 
-
-        // Opening door
-        OPEN:
+        else
         begin
 
-            door_open <= 1;
-            door_busy <= 1;
+            case(state)
 
-            timer <= 3;
 
-            state <= WAIT;
+
+            // Door closed state
+            CLOSED:
+            begin
+
+                door_open <= 1'b0;
+                counter <= 32'd0;
+
+
+                if(open_request)
+                    state <= OPEN;
+
+            end
+
+
+
+            // Door opening state
+            OPEN:
+            begin
+
+                door_open <= 1'b1;
+
+                state <= WAIT;
+
+            end
+
+
+
+            // Door open for 5 seconds
+            WAIT:
+            begin
+
+                door_open <= 1'b1;
+
+                counter <= counter + 1;
+
+
+                if(counter == 32'd250000000)
+                begin
+
+                    door_open <= 1'b0;
+
+                    counter <= 32'd0;
+
+                    state <= CLOSED;
+
+                end
+
+            end
+
+
+
+            endcase
 
         end
-
-
-
-        // Door waiting state
-        WAIT:
-        begin
-
-            door_open <= 1;
-
-            if(obstruction)
-            begin
-                timer <= 3;
-            end
-
-
-            else if(timer > 0)
-            begin
-                timer <= timer - 1;
-            end
-
-
-            else
-            begin
-                state <= CLOSE;
-            end
-
-        end
-
-
-
-        // Closing door
-        CLOSE:
-        begin
-
-            door_close <= 1;
-            door_open <= 0;
-            door_busy <= 0;
-
-
-            if(obstruction)
-            begin
-                state <= OPEN;
-            end
-
-
-            else
-            begin
-                state <= CLOSED;
-            end
-
-        end
-
-
-        endcase
 
     end
-
-end
 
 
 endmodule
