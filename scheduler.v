d073846a9f7692f6e2ae66cfefb09a1ad2848493
Akshara2007 <perkaakshara3@gmail.com>
2026-07-15 19:53:59 +0530

    Update scheduler.v

--- scheduler.v
+++ scheduler.v
@@ -1,1 +1,47 @@
+`timescale 1ns/1ps
 
+module scheduler(
+
+    input [3:0] requests,
+    input [1:0] current_floor,
+
+    output reg [1:0] target_floor,
+    output reg request_available
+
+);
+
+always @(*)
+begin
+
+    request_available = 1'b0;
+    target_floor = current_floor;
+
+    if(requests[current_floor])
+    begin
+        target_floor = current_floor;
+        request_available = 1'b1;
+    end
+    else if(requests[0])
+    begin
+        target_floor = 2'b00;
+        request_available = 1'b1;
+    end
+    else if(requests[1])
+    begin
+        target_floor = 2'b01;
+        request_available = 1'b1;
+    end
+    else if(requests[2])
+    begin
+        target_floor = 2'b10;
+        request_available = 1'b1;
+    end
+    else if(requests[3])
+    begin
+        target_floor = 2'b11;
+        request_available = 1'b1;
+    end
+
+end
+
+endmodule
